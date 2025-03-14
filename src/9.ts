297a5fe496420423353d82e49cc364b2812fd7e8
contamanayojhany <contamanayojhany@gmail.com>
2025-03-14 13:27:17 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,27 @@
+ import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
+}
