fa8654a9d52b2504ace21f46a5e4f3572fb505c3
contamanayojhany <contamanayojhany@gmail.com>
2025-04-03 14:34:54 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 3;
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(addNumbers(num1, num2)); // Output: 8
+
+addNumbers(7, -4); // Output: 3
