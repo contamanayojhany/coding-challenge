4c337c97b07cc9a547ee92eff1bdba5c90b6d384
contamanayojhany <contamanayojhany@gmail.com>
2025-04-23 10:57:24 +0000

    Add 29.ts

---
+++ src/29.ts
@@ -0,0 +1,8 @@
+let x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 3) {
+    console.log("x is less than 3");
+} else {
+    console.log("x is between 5 and 3, or undefined");
+}
